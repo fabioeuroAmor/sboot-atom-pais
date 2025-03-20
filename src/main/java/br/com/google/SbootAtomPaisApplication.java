1e1f3b4eea1ef89382cf7e77f801ea25f44d9c71
FABIO MONTEIRO AMORIM <fmamorim_ext@grupogbi.com>
2025-03-20 20:22:09 -0300

    subinda de arquitetura inicial

---
+++ src/main/java/br/com/google/SbootAtomPaisApplication.java
@@ -0,0 +1,13 @@
+package br.com.google;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootAtomPaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootAtomPaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/google/SbootAtomPaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.google;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootAtomPaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
