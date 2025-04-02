32e2525080964a3970fa182e401e8f61eb662ab3
FABIO MONTEIRO AMORIM <fmamorim_ext@grupogbi.com>
2025-04-02 19:52:03 -0300

    corrigido

--- src/main/java/br/com/google/controller/PaisController.java
+++ src/main/java/br/com/google/controller/PaisController.java
@@ -18,9 +18,6 @@ public class PaisController {
     @Autowired
     private PaisService paisService;
 
-
-
-
     @PostMapping()
     public ResponseEntity<Response> inserirPais(@RequestParam(value = "nome", required = true) String nome, @RequestParam(value = "codigo", required = true) String codigo, @RequestParam(value = "continente", required = true) String continente) {
         Response response = new Response();
@@ -47,7 +44,7 @@ public class PaisController {
     }
 
     @PostMapping("/body")
-    public  ResponseEntity<Response> inserirPais(@RequestBody PaisDto paisDto) {
+    public ResponseEntity<Response> inserirPais(@RequestBody PaisDto paisDto) {
         Response response = new Response();
         PaisDto paisServDto = null;
 
@@ -70,32 +67,32 @@ public class PaisController {
         }
         return ResponseEntity.ok(response);
     }
-         //paisServDto = new PaisDto();
-         //paisServDto.setNome(paisDto.getNome());
-         //paisServDto.setCodigo(paisDto.getCodigo());
-         //paisServDto.setContinente(paisDto.getContinente());
-}
 
-@GetMapping
-public ResponseEntity<Response> consulta(@RequestParam (value = "idPais", required = false) Integer idPais, @RequestParam (value = "nome", required = false) String nome, @RequestParam (value = "codigo", required = false) String codigo, @RequestParam (value = "continente", required = false) String continente) {
-    try {
-        paisDto = new PaisDto();
-        paisServDto.setIdPais(idPais);
-        paisServDto.setNome(nome);
-        paisServDto.setCodigo(codigo);
-        paisServDto.setContinente(continente);
 
-        response.setModeloRetorno(paisServDto);
-        response.setModeloRetorno("Insercao do pais foi com sucesso!");
+    @GetMapping
+    public ResponseEntity<Response> consulta(@RequestParam(value = "idPais", required = false) Integer idPais, @RequestParam(value = "nome", required = false) String nome, @RequestParam(value = "codigo", required = false) String codigo, @RequestParam(value = "continente", required = false) String continente) {
 
-        //paisService.
-        //response.setModeloRetorno(paisService.inserirPais(paisDto));
+        Response response = new Response();
+        try {
+            PaisDto paisServDto = new PaisDto();
+            paisServDto.setIdPais(idPais);
+            paisServDto.setNome(nome);
+            paisServDto.setCodigo(codigo);
+            paisServDto.setContinente(continente);
 
-    } catch (Exception e) {
-        log.error("Erro ao inserir o pais: " + e.getMessage());
-        response.setMensagensRetorno(e.getMessage());
-        return (ResponseEntity<Response>) ResponseEntity.status(500);
+            response.setModeloRetorno(paisServDto);
+            response.setModeloRetorno("Insercao do pais foi com sucesso!");
+
+            //paisService.
+            //response.setModeloRetorno(paisService.inserirPais(paisDto));
+
+        } catch (Exception e) {
+            log.error("Erro ao inserir o pais: " + e.getMessage());
+            response.setMensagensRetorno(e.getMessage());
+            return (ResponseEntity<Response>) ResponseEntity.status(500);
+        }
+        return ResponseEntity.ok(response);
+
     }
-    return ResponseEntity.ok(response);
 
 }
